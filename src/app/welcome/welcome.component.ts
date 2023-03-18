214e79a74c94abf6a3be1495005fa7637ea3257c
Rajnikant <Rajnikantbhosale15@gmail.com>
2023-03-19 00:48:21 +0530

    'Done'

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
@@ -11,4 +11,17 @@ export class WelcomeComponent implements OnInit {
   ngOnInit(): void {}
   bgVariable: boolean = false;
   headerVariable: boolean = false;
+  @HostListener('document:scroll')
+  onScroll() {
+    if (
+      document.body.scrollTop > 60 ||
+      document.documentElement.scrollTop > 60
+    ) {
+      this.bgVariable = true;
+      this.headerVariable = true;
+    } else {
+      this.bgVariable = false;
+      this.headerVariable = false;
+    }
+  }
 }
